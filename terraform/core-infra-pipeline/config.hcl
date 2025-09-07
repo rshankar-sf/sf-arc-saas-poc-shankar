85a26c1de082627e6d2702115f40623c3bc6cfab
rshankar-sf <shankar.r@sourcefuse.com>
2025-09-08 03:57:17 +0530

    adding new code for us-east-1

--- terraform/core-infra-pipeline/config.hcl
+++ terraform/core-infra-pipeline/config.hcl
@@ -1,5 +1,5 @@
 region         = "us-east-1"
 key            = "codepipeline/terraform.tfstate"
-bucket         = "sf-arc-saas-poc-terraform-state-fphnka"
+bucket         = "sf-arc-saas-poc-terraform-state-5dqm7e"
 dynamodb_table = "sf-arc-saas-poc-terraform-state-lock"
 encrypt        = true
--- terraform/core-infra-pipeline/variables.tf
+++ terraform/core-infra-pipeline/variables.tf
@@ -9,14 +9,14 @@ variable "region" {
 
 variable "environment" {
   type        = string
-  default     = "dev"
+  default     = "poc"
   description = "ID element. Usually used for region e.g. 'uw2', 'us-west-2', OR role 'prod', 'staging', 'dev', 'UAT'"
 }
 
 variable "namespace" {
   type        = string
   description = "Namespace for the resources."
-  default     = "arc-saas"
+  default     = "sf-arc-saas"
 }
 
 ################################################################
